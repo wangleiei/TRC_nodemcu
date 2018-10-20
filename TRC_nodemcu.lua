2bced7f3367fe75cf0409829f5c0d6b21c2dc06d
wangleiei <2350235394@qq.com>
2018-10-20 19:04:43 +0800

    第一次提交

---
+++ TRC_nodemcu.lua
@@ -0,0 +1,27 @@
+pumppin = 2
+flag_pumpon = 9
+
+rh = 40
+
+function gettemphum(...) rh = rh+1 return 223,rh end
+function pumpon()gpio.mode(pumppin,gpio.OUTPUT) gpio.write(pumppin,gpio.LOW) print("pumpon") end
+function pumpoff()gpio.mode(pumppin,gpio.OUTPUT) gpio.write(pumppin,gpio.HIGH) print("pumpoff") end
+-- in 5 minutes dont water
+function pumpondeal( ... ) 
+	if(flag_pumpon != 1)then
+		pumpon()
+		flag_pumpon = 1
+		pumpontimer = tmr.create()
+		pumpontimer:register(5000, tmr.ALARM_SINGLE, function() flag_pumpon = 0 end end)
+		pumpontimer:start()
+	end
+end
+
+tp1 rh1 = gettemphum()
+-- 湿度小于x water 1 secnond
+if(rh1 < 67)then 
+	pumpondeal()
+	pumpofftime = tmr.create()
+	pumpofftime:register(2000, tmr.ALARM_SINGLE, function() pumpoff end)
+	pumpofftime:start()	
+end
